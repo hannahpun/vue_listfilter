1bcb05ac0c780bf0addd780bac0a88a63f4c9bea
hannahpun <hannahpun@gmail.com>
2018-06-14 10:07:34 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue_listfilte/',
+    assetsPublicPath: '/vue_listfilter/',
 
     /**
      * Source Maps
